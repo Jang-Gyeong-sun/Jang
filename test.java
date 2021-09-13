e540f5e616d34e32de4317e4884f63f566dc5af0
JangGyeongSun <jks4745@naver.com>
2021-09-14 08:14:19 +0900

    msg

--- test.java
+++ test.java
@@ -1,36 +1,20 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.lang.reflect.Array;
+import java.util.ArrayList;
+import java.util.Iterator;
+
+
 
 public class test {
 
     public static void main(String[] args) throws IOException{
-        TestIf testif = new TestIfClass();
+      
+        System.out.println("test msg");
 
-        testif.TestMethod();
-
-        String str = "123";
-        
-        Integer.parseInt(str);
-
-        System.out.println(str.length());
-
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        
-        System.out.println(br.readLine());
     }
 
     
     
-}
-
-interface TestIf{
-    void TestMethod();
-}
-
-class TestIfClass implements TestIf{
-    
-    public void TestMethod(){
-        System.out.println("Test class");
-    }
 }
